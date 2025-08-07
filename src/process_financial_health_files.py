9515398538970f092ebe1a2e8255d43e2bdad912
Doug Roehm <158538535+DougRoehm@users.noreply.github.com>
2025-08-07 12:21:53 -0600

    Fix formatting and typos

--- src/process_financial_health_files.py
+++ src/process_financial_health_files.py
@@ -1,5 +1,5 @@
 """
-Used to process a folder containing Morningstar Financial Health Exports
+Used to process a folder containing Morningstar Financial Health exports
 """
 
 import os
@@ -16,19 +16,16 @@ industry_description = ""
 gpc_tickers = []
 file_names = []
 
-
 # Collect user information
 industry_description = input("Enter industry description:\n").lower().replace(" ", "-")
 folder_path = input("Enter folder path:\n")
 file_paths = get_files_from_folder(folder_path)
-
 
 # Root, source, and output folders
 current_file = pathlib.Path(__file__).resolve()
 project_root = current_file.parent.parent
 src_folder = project_root / 'src'
 output_folder = project_root / "output"
-
 
 # Process files using financial health functions and save to DataFrame
 df_ratios = pd.DataFrame()
@@ -37,11 +34,9 @@ for file in file_paths:
     df_temp = process_financial_health_export(file)
     df_ratios = pd.concat([df_ratios, df_temp], axis = 0)
 
-
 # Output file name and path
 output_file_name = f"gpc-financial-health-{industry_description}.csv"
 output_file_path = output_folder / output_file_name
-
 
 # Save file
 df_ratios.to_csv(output_file_path)
